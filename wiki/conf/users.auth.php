bb79e54011680f04951d836152ae0ac3cf96daac
Roger Qiu <roger.qiu@polycademy.com>
2012-10-12 02:23:20 +1100

    A new beginning for Polycademy
    Remember that the wiki is not updated, unlike the hosted wiki on
    aiesecanu.org. Furthermore on the .htaccess file, there are conditions
    regarding PHP security, you need to check that.

---
+++ wiki/conf/users.auth.php
@@ -0,0 +1,13 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# user:MD5password:Real Name:email:groups,comma,seperated
+
+
+cmcdragonkai:cd185c0ebcd2cb1eeb8d0d6559758921:Roger Qiu:cmcdragonkai@gmail.com:admin,user
+ameelbeesony:$1$yPvlKcg3$x4S3t7287PkFLHwH1R54f/:Ameel Beesony:ameel.beesony@gmail.com:admin,user
