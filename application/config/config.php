3c5ffd58fe73a38ebf68e4c41a6baa749f6f510c
Roger Qiu <roger.qiu@polycademy.com>
2012-10-24 00:41:14 +1100

    Key
    This key needs to be changed when you host it. Or you need a private
    repository.

--- application/config/config.php
+++ application/config/config.php
@@ -258,7 +258,7 @@ $config['cache_path'] = '';
 | http://codeigniter.com/user_guide/libraries/sessions.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'jf8493UI56390LK432ghfj3dcH45345g';
 
 /*
 |--------------------------------------------------------------------------
