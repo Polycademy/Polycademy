3b2c0a1847c2c084f050b595b95588678192e222
Roger Qiu <roger.qiu@polycademy.com>
2012-10-24 16:14:29 +1100

    Adding in placeholder pages for all the links.

---
+++ application/views/courses_view.php
@@ -0,0 +1,10 @@
+		<div class="article_slider_container">
+			<div class="container">
+				<article class="main_article">
+					<h2>We are currently still in development.</h2>
+					<h4>This page will list all the course modules, dates, hours, features, cost and signup form.</h4>
+					<img style="display: block; margin: 20px auto;" src="http://www.lolcats.com/images/u/08/32/lolcatsdotcombkf8azsotkiwu8z2.jpg" />
+					<h2 id="call_to_action">Please check back for later.</h2>
+				</article>
+			</div>
+		</div>
