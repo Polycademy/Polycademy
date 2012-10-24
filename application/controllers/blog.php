61c128f3746aa7af0e9632faf76eb4f57dd0021d
Roger Qiu <roger.qiu@polycademy.com>
2012-10-24 16:22:26 +1100

    Changing placeholder pages and adding details
    Created old_files for anything no long required, but kept as historical
    data.

---
+++ application/controllers/blog.php
@@ -0,0 +1,56 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Blog extends CI_Controller {
+
+	protected $_view_data; //only accessible in this class and any classes that extend home
+	protected $_settings;
+	
+	public function __construct(){
+		parent::__construct();
+		$this->_settings = $this->config->item('polycademy');
+		$this->load->library('firephp');
+		#construct for Home class set some default values, or run a default process when your class is instantiated
+		#this stuff is executed regardless of what page is loaded, good for when index()is not launched
+	}
+	
+	public function index(){
+	
+		$rss_feeds = rss_process('http://pipes.yahoo.com/pipes/pipe.run?_id=24a7ee6208f281f8dff1162dbac57584&_render=rss');
+		if($rss_feeds){
+			$rss_feeds = array_slice($rss_feeds, 0, 4);
+		}
+		
+		$this->_view_data = $this->_settings;
+		$this->_view_data += array(
+			'page_title'			=> 'Blog',
+			'page_desc'				=> $this->_settings['site_desc'],
+			'feeds'					=> $rss_feeds,
+		);
+		
+		$this->load->view('header_view', $this->_view_data);
+		$this->load->view('blog_view', $this->_view_data);
+		$this->load->view('footer_view', $this->_view_data);
+		
+	}
+	
+	public function notices(){
+	
+		$rss_feeds = rss_process('http://pipes.yahoo.com/pipes/pipe.run?_id=24a7ee6208f281f8dff1162dbac57584&_render=rss');
+		if($rss_feeds){
+			$rss_feeds = array_slice($rss_feeds, 0, 4);
+		}
+		
+		$this->_view_data = $this->_settings;
+		$this->_view_data += array(
+			'page_title'			=> 'Blog',
+			'page_desc'				=> $this->_settings['site_desc'],
+			'feeds'					=> $rss_feeds,
+		);
+		
+		$this->load->view('header_view', $this->_view_data);
+		$this->load->view('blog_view', $this->_view_data);
+		$this->load->view('footer_view', $this->_view_data);
+		
+	}
+	
+}
