7284b1e391e5b90d0cf42f5b8390baaa56bb3538
Roger Qiu <roger.qiu@polycademy.com>
2012-10-24 16:26:43 +1100

    Added some lolcat pics for placeholders.

--- application/views/alumni_view.php
+++ application/views/alumni_view.php
@@ -3,7 +3,7 @@
 				<article class="main_article">
 					<h2>We are currently still in development.</h2>
 					<h4>This page will list all the course modules, dates, hours, features, cost and signup form.</h4>
-					<img style="display: block; margin: 20px auto;" src="http://www.lolcats.com/images/u/08/32/lolcatsdotcombkf8azsotkiwu8z2.jpg" />
+					<img style="display: block; margin: 20px auto;" src="http://www.lolcats.com/images/u/08/46/lolcatsdotcomj4wgx1z66imduk9x.jpg" />
 					<h2 id="call_to_action">Please check back for later.</h2>
 				</article>
 			</div>
--- application/views/blog_view.php
+++ application/views/blog_view.php
@@ -3,7 +3,7 @@
 				<article class="main_article">
 					<h2>We are currently still in development.</h2>
 					<h4>This page will list all the course modules, dates, hours, features, cost and signup form.</h4>
-					<img style="display: block; margin: 20px auto;" src="http://www.lolcats.com/images/u/08/32/lolcatsdotcombkf8azsotkiwu8z2.jpg" />
+					<img style="display: block; margin: 20px auto;" src="http://www.lolcats.com/images/u/08/46/lolcatsdotcombsr71ehdl7l9ocf2.jpg" />
 					<h2 id="call_to_action">Please check back for later.</h2>
 				</article>
 			</div>
