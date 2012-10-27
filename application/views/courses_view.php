5c52c8fb7da2d6aee7336699903693f604f35f3a
Roger Qiu <roger.qiu@polycademy.com>
2012-10-28 03:37:27 +1100

    For the new course view

--- application/views/courses_view.php
+++ application/views/courses_view.php
@@ -1,10 +1,18 @@
-		<div class="article_slider_container">
+		<div class="blue_container">
 			<div class="container">
 				<article class="main_article">
-					<h2>We are currently still in development.</h2>
+					<h2>Six Months Standard</h2>
 					<h4>This page will list all the course modules, dates, hours, features, cost and signup form.</h4>
-					<img style="display: block; margin: 20px auto;" src="http://www.lolcats.com/images/u/08/32/lolcatsdotcombkf8azsotkiwu8z2.jpg" />
-					<h2 id="call_to_action">Please check back for later.</h2>
+					<h2>Please check back for later.</h2>
+				</article>
+			</div>
+		</div>
+		<div class="red_container">
+			<div class="container">
+				<article class="main_article">
+					<h2>Three Months Express</h2>
+					<h4>This page will list all the course modules, dates, hours, features, cost and signup form.</h4>
+					<h2>Please check back for later.</h2>
 				</article>
 			</div>
 		</div>
