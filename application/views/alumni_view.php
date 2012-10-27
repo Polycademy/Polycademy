2caad13ab79bdbcbfccafe45ff083d7eb6e016ce
Roger Qiu <roger.qiu@polycademy.com>
2012-10-28 04:10:40 +1100

    Fixed some html in the other pages

--- application/views/alumni_view.php
+++ application/views/alumni_view.php
@@ -1,6 +1,6 @@
-		<div class="article_slider_container">
+		<div class="blue_container">
 			<div class="container">
-				<article class="main_article">
+				<article>
 					<h2>We are currently still in development.</h2>
 					<h4>This page will list all the course modules, dates, hours, features, cost and signup form.</h4>
 					<img style="display: block; margin: 20px auto;" src="http://www.lolcats.com/images/u/08/46/lolcatsdotcomj4wgx1z66imduk9x.jpg" />
--- application/views/courses_view.php
+++ application/views/courses_view.php
@@ -1,6 +1,6 @@
 		<div class="blue_container">
 			<div class="container">
-				<article class="main_article">
+				<article>
 					<h2>Six Months Standard</h2>
 					<h4>This page will list all the course modules, dates, hours, features, cost and signup form.</h4>
 					<h2>Please check back for later.</h2>
@@ -9,7 +9,7 @@
 		</div>
 		<div class="red_container">
 			<div class="container">
-				<article class="main_article">
+				<article>
 					<h2>Three Months Express</h2>
 					<h4>This page will list all the course modules, dates, hours, features, cost and signup form.</h4>
 					<h2>Please check back for later.</h2>
