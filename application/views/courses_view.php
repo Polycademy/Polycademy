46e589c8454e066f595f1c26c67cbb514ea608c1
Roger Qiu <roger.qiu@polycademy.com>
2012-10-28 21:49:32 +1100

    Committing 6 months standard description

--- application/views/courses_view.php
+++ application/views/courses_view.php
@@ -1,152 +1,53 @@
 		<div class="blue_container">
 			<div class="container">
 				<article class="liquid-slider" id="course_slider">
-					<section class="envision_slide">
-						<h3 class="slide_title">6 Months Standard</h3>
+					<section class="six_months_slide">
+						<h1 class="slide_title">6 Months Standard</h1>
 						<div class="slide_grid">
 							<div class="row-fluid">
-								<section class="slide_details span4">
-									<div class="row-fluid">
-										<div class="span4">
-											<img src="<?php echo base_url($links['img_assets']) ?>/detailed_hex_network.png" alt="" />
-										</div>
-										<div class="span8">
-											<h5>Inspiration</h5>
-											<p>Students come with different backgrounds and locations, which allows a cross pollination of ideas.</p>
-										</div>
-									</div>
+								<section class="slide_details span6">
+									<h4>What is it?</h4>
+									<p>The 6 months standard involves 7 hours per week instruction time plus 3 hours per week free time. The class size is limited to 15 students and we recommend you to get into a team of 2 or 3. During after hours, the classroom will be open for you to chill out and meet the other students.</p>
+									<p>Our three philosophies at Polycademy is <strong>Minimum Viable Product (MVP)</strong>, <strong>Agile Development</strong> and <strong>Flipped Classroom</strong>.</p>
+									<ul>
+										<li><strong>MVP</strong> means designing your product so it is minimum enough to launch quickly to get market testing, and viable enough to make your product distinct from competitors.</li>
+										<li><strong>Agile Development</strong> is about rapid and flexible incremental iteration to code. Rather than memorising the whole corpus of programming in 6 months, each week is a milestone in which parts of your product will be deployed.</li>
+										<li><strong>Flipped Classroom</strong> means that before class an online ecosystem of materials will be provided to you. During class we'll keep it practical by spending time discussing models, cutting code, and deploying applications to the cloud.</li>
+									</ul>
+									<p>We'll be arranging your team with a mentor who will volunteer their time to coach your team on the entrepenurial or development process.</p>
+									<p>At the end of 24 weeks, a careers/pitching fair will be opened. Tech employers and investors will be invited to check out your work.</p>
 								</section>
-								<section class="slide_details span4">
-									<div class="row-fluid">
-										<div class="span4">
-											<img src="<?php echo base_url($links['img_assets']) ?>/detailed_hex_team.png" alt="" />
-										</div>
-										<div class="span8">
-											<h5>Teamwork</h5>
-											<p>Develop your soft skills in communication, delegation, collaboration and team building.</p>
-										</div>
-									</div>
-								</section>
-								<section class="slide_details span4">
-									<div class="row-fluid">
-										<div class="span4">
-											<img src="<?php echo base_url($links['img_assets']) ?>/detailed_hex_mvp.png" alt="" />
-										</div>
-										<div class="span8">
-											<h5>MVP</h5>
-											<p>Design based on minimum viable product so you can test your idea’s commercial potential quickly.</p>
-										</div>
-									</div>
+								<section class="slide_details span6">
+									<h4>What will you learn?</h4>
 								</section>
 							</div>
 							<div class="row-fluid">
-								<section class="slide_details span4">
-									<div class="row-fluid">
-										<div class="span4">
-											<img src="<?php echo base_url($links['img_assets']) ?>/detailed_hex_copyleft.png" alt="" />
-										</div>
-										<div class="span8">
-											<h5>Intellectual Property</h5>
-											<p>Everything you build is yours. We’ll use Github to host your open source code for free.</p>
-										</div>
-									</div>
+								<section class="slide_details span6">
+									<h4>When will you start?</h4>
 								</section>
-								<section class="slide_details span4">
-									<div class="row-fluid">
-										<div class="span4">
-											<img src="<?php echo base_url($links['img_assets']) ?>/detailed_hex_stack.png" alt="" />
-										</div>
-										<div class="span8">
-											<h5>Solution Stack</h5>
-											<p>Learn how to setup the development environment and the solution stack to run your app.</p>
-										</div>
-									</div>
-								</section>
-								<section class="slide_details span4">
-									<div class="row-fluid">
-										<div class="span4">
-											<img src="<?php echo base_url($links['img_assets']) ?>/detailed_hex_australia.png" alt="" />
-										</div>
-										<div class="span8">
-											<h5>Code for Australia</h5>
-											<p>Get involved in building applications for open gov and community organisations.</p>
-										</div>
-									</div>
+								<section class="slide_details span6">
+									<h4>Ready?</h4>
 								</section>
 							</div>
 						</div>
 					</section>
-					<section class="build_slide">
-						<h3 class="slide_title">3 Months Express</h3>
+					<section class="three_months_slide">
+						<h1 class="slide_title">3 Months Express</h1>
 						<div class="slide_grid">
 							<div class="row-fluid">
-								<section class="slide_details span4">
-									<div class="row-fluid">
-										<div class="span4">
-											<img src="<?php echo base_url($links['img_assets']) ?>/detailed_hex_html.png" alt="" />
-										</div>
-										<div class="span8">
-											<h5>HTML5 & CSS3</h5>
-											<p>Make use of the semantic web and bend this ubiquitous web platform to your will.</p>
-										</div>
-									</div>
+								<section class="slide_details span6">
+									<h4>Description</h4>
 								</section>
-								<section class="slide_details span4">
-									<div class="row-fluid">
-										<div class="span4">
-											<img src="<?php echo base_url($links['img_assets']) ?>/detailed_hex_js.png" alt="" />
-										</div>
-										<div class="span8">
-											<h5>Javascript</h5>
-											<p>Go interactive with Javascript, jQuery, and CoffeeScript to make your web app come to life.</p>
-										</div>
-									</div>
-								</section>
-								<section class="slide_details span4">
-									<div class="row-fluid">
-										<div class="span4">
-											<img src="<?php echo base_url($links['img_assets']) ?>/detailed_hex_php.png" alt="" />
-										</div>
-										<div class="span8">
-											<h5>Codeigniter PHP</h5>
-											<p>Use PHP & the Codeigniter framework to write industry standard MVC applications.</p>
-										</div>
-									</div>
+								<section class="slide_details span6">
+									<h4>Details</h4>
 								</section>
 							</div>
 							<div class="row-fluid">
-								<section class="slide_details span4">
-									<div class="row-fluid">
-										<div class="span4">
-											<img src="<?php echo base_url($links['img_assets']) ?>/detailed_hex_performance.png" alt="" />
-										</div>
-										<div class="span8">
-											<h5>Performance</h5>
-											<p>Compress, cache, refactor and use CDNs to make your app zoom!</p>
-										</div>
-									</div>
+								<section class="slide_details span6">
+									<h4>Timeline</h4>
 								</section>
-								<section class="slide_details span4">
-									<div class="row-fluid">
-										<div class="span4">
-											<img src="<?php echo base_url($links['img_assets']) ?>/detailed_hex_security.png" alt="" />
-										</div>
-										<div class="span8">
-											<h5>Security</h5>
-											<p>Learn how to authenticate requests, secure passwords, and protect against common attacks.</p>
-										</div>
-									</div>
-								</section>
-								<section class="slide_details span4">
-									<div class="row-fluid">
-										<div class="span4">
-											<img src="<?php echo base_url($links['img_assets']) ?>/detailed_hex_github.png" alt="" />
-										</div>
-										<div class="span8">
-											<h5>Git Source Control</h5>
-											<p>Use Git to collaborate and version control code, and get developer cred through social coding at Github.</p>
-										</div>
-									</div>
+								<section class="slide_details span6">
+									<h4>Ready?</h4>
 								</section>
 							</div>
 						</div>
@@ -157,9 +58,9 @@
 		<div class="red_container">
 			<div class="container">
 				<article>
-					<h2>Three Months Express</h2>
-					<h4>This page will list all the course modules, dates, hours, features, cost and signup form.</h4>
-					<h2>Please check back for later.</h2>
+					<h2>Application Form</h2>
+					<h4>Becoming a web application entrepeneur is not easy. You need to be truly passionate and committed to developing your idea into reality.</h4>
+					<h2>GO!</h2>
 				</article>
 			</div>
 		</div>
