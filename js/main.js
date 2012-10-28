3f781272c3c65e87b1a2ebeeb1a811646ac212d6
Roger Qiu <roger.qiu@polycademy.com>
2012-10-28 21:48:50 +1100

    Combined plugins.js with main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+/* ==========================================================================
+   PLUGINS
+   ========================================================================== */
+// Avoid `console` errors in browsers that lack a console.
+if (!(window.console && console.log)) {
+    (function() {
+        var noop = function() {};
+        var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'markTimeline', 'table', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn'];
+        var length = methods.length;
+        var console = window.console = {};
+        while (length--) {
+            console[methods[length]] = noop;
+        }
+    }());
+}
+
+/* ==========================================================================
+   MAIN JAVASCRIPT
+   ========================================================================== */
 $(function(){
 
 	// Equalise column height for any plurality of rows, just make sure to give a selector that has multiple elements
