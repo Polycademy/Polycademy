a437395b43b5170564bd0013c0d29db54e52b684
Roger Qiu <roger.qiu@polycademy.com>
2012-10-30 20:24:59 +1100

    Configured cookies to have a unique prefix

--- application/config/config.php
+++ application/config/config.php
@@ -305,7 +305,7 @@ $config['sess_time_to_update']	= 300;
 | 'cookie_httponly' = Cookie will only be accessible via HTTP(S) (no javascript)
 |
 */
-$config['cookie_prefix']	= '';
+$config['cookie_prefix']	= 'polycademy_';
 $config['cookie_domain']	= '';
 $config['cookie_path']		= '/';
 $config['cookie_secure']	= FALSE;
@@ -336,9 +336,9 @@ $config['global_xss_filtering'] = FALSE;
 | 'csrf_regenerate' = Regenerate token on every submission
 | 'csrf_exclude_uris' = Array of URIs which ignore CSRF checks
 */
-$config['csrf_protection'] = FALSE;
-$config['csrf_token_name'] = 'csrf_test_name';
-$config['csrf_cookie_name'] = 'csrf_cookie_name';
+$config['csrf_protection'] = TRUE;
+$config['csrf_token_name'] = 'csrf_test_polycademy';
+$config['csrf_cookie_name'] = 'csrf_cookie_polycademy';
 $config['csrf_expire'] = 7200;
 $config['csrf_regenerate'] = TRUE;
 $config['csrf_exclude_uris'] = array();
