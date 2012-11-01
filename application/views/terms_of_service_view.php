334d2aaf1dad2d999d171cc4c98e7c565c6d7c91
Roger Qiu <roger.qiu@polycademy.com>
2012-11-02 01:09:33 +1100

    Adjusted TOS, PP, and Refund

--- application/views/terms_of_service_view.php
+++ application/views/terms_of_service_view.php
@@ -2,38 +2,53 @@
 			<div class="container">
 				<article class="terms_of_service blue_text_container">
 					<h2>Terms of Service</h2>
-					<h4>1. Terms</h4>
-					<p>By accessing this web site, you are agreeing to be bound by these web site Terms and Conditions of Use, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this site. The materials contained in this web site are protected by applicable copyright and trade mark law.</p>
-					<h4>2. Use License</h4>
-					<ol type="a">
-						<li>
-						Permission is granted to temporarily download one copy of the materials (information or software) on Polycademy's web site for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license you may not:
-							<ol type="i">
-								<li>modify or copy the materials;</li>
-								<li>use the materials for any commercial purpose, or for any public display (commercial or non-commercial);</li>
-								<li>attempt to decompile or reverse engineer any software contained on Polycademy's web site;</li>
-								<li>remove any copyright or other proprietary notations from the materials; or</li>
-								<li>transfer the materials to another person or "mirror" the materials on any other server.</li>
-							</ol>
-						</li>
-						<li>This license shall automatically terminate if you violate any of these restrictions and may be terminated by Polycademy at any time. Upon terminating your viewing of these materials or upon the termination of this license, you must destroy any downloaded materials in your possession whether in electronic or printed format.</li>
-					</ol>
-					<h4>3. Disclaimer</h4>
-					<ol type="a">
-						<li>The materials on Polycademy's web site are provided "as is". Polycademy makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties, including without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights. Further, Polycademy does not warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials on its Internet web site or otherwise relating to such materials or on any sites linked to this site.</li>
-					</ol>
-					<h4>4. Limitations</h4>
-					<p>In no event shall Polycademy or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption,) arising out of the use or inability to use the materials on Polycademy's Internet site, even if Polycademy or a Polycademy authorized representative has been notified orally or in writing of the possibility of such damage. Because some jurisdictions do not allow limitations on implied warranties, or limitations of liability for consequential or incidental damages, these limitations may not apply to you.
-					</p>
-					<h4>5. Revisions and Errata</h4>
-					<p>The materials appearing on Polycademy's web site could include technical, typographical, or photographic errors. Polycademy does not warrant that any of the materials on its web site are accurate, complete, or current. Polycademy may make changes to the materials contained on its web site at any time without notice. Polycademy does not, however, make any commitment to update the materials.</p>
-					<h4>6. Links</h4>
-					<p>Polycademy has not reviewed all of the sites linked to its Internet web site and is not responsible for the contents of any such linked site. The inclusion of any link does not imply endorsement by Polycademy of the site. Use of any such linked web site is at the user's own risk.</p>
-					<h4>7. Site Terms of Use Modifications</h4>
-					<p>Polycademy may revise these terms of use for its web site at any time without notice. By using this web site you are agreeing to be bound by the then current version of these Terms and Conditions of Use.</p>
-					<h4>8. Governing Law</h4>
-					<p>Any claim relating to Polycademy's web site shall be governed by the laws of the State of ACT without regard to its conflict of law provisions.</p>
 					<p>Effective date: October 30th 2012</p>
+					<section>
+						<h4>1. Terms</h4>
+						<p>By accessing this web site, you are agreeing to be bound by these web site Terms and Conditions of Use, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this site. The materials contained in this web site are protected by applicable copyright and trade mark law.</p>
+					</section>
+					<section>
+						<h4>2. Use License</h4>
+						<ol type="a">
+							<li>Permission is granted to temporarily download one copy of the materials (information or software) on Polycademy's web site for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license you may not:
+								<ol type="i">
+									<li>modify or copy the materials;</li>
+									<li>use the materials for any commercial purpose, or for any public display (commercial or non-commercial);</li>
+									<li>attempt to decompile or reverse engineer any software contained on Polycademy's web site;</li>
+									<li>remove any copyright or other proprietary notations from the materials; or</li>
+									<li>transfer the materials to another person or "mirror" the materials on any other server.</li>
+								</ol>
+							</li>
+							<li>This license shall automatically terminate if you violate any of these restrictions and may be terminated by Polycademy at any time. Upon terminating your viewing of these materials or upon the termination of this license, you must destroy any downloaded materials in your possession whether in electronic or printed format.</li>
+						</ol>
+					</section>
+					<section>
+						<h4>3. Disclaimer</h4>
+						<ol type="a">
+							<li>The materials on Polycademy's web site are provided "as is". Polycademy makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties, including without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights. Further, Polycademy does not warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials on its Internet web site or otherwise relating to such materials or on any sites linked to this site.</li>
+						</ol>
+					</section>
+					<section>
+						<h4>4. Limitations</h4>
+						<p>In no event shall Polycademy or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption,) arising out of the use or inability to use the materials on Polycademy's Internet site, even if Polycademy or a Polycademy authorized representative has been notified orally or in writing of the possibility of such damage. Because some jurisdictions do not allow limitations on implied warranties, or limitations of liability for consequential or incidental damages, these limitations may not apply to you.
+						</p>
+					</section>
+					<section>
+						<h4>5. Revisions and Errata</h4>
+						<p>The materials appearing on Polycademy's web site could include technical, typographical, or photographic errors. Polycademy does not warrant that any of the materials on its web site are accurate, complete, or current. Polycademy may make changes to the materials contained on its web site at any time without notice. Polycademy does not, however, make any commitment to update the materials.</p>
+					</section>
+					<section>
+						<h4>6. Links</h4>
+						<p>Polycademy has not reviewed all of the sites linked to its Internet web site and is not responsible for the contents of any such linked site. The inclusion of any link does not imply endorsement by Polycademy of the site. Use of any such linked web site is at the user's own risk.</p>
+					</section>
+					<section>
+						<h4>7. Site Terms of Use Modifications</h4>
+						<p>Polycademy may revise these terms of use for its web site at any time without notice. By using this web site you are agreeing to be bound by the then current version of these Terms and Conditions of Use.</p>
+					</section>
+					<section>
+						<h4>8. Governing Law</h4>
+						<p>Any claim relating to Polycademy's web site shall be governed by the laws of the State of ACT without regard to its conflict of law provisions.</p>
+					</section>
 				</article>
 			</div>
 		</div>
@@ -41,6 +56,7 @@
 			<div class="container">
 				<article class="privacy_policy red_text_container">
 					<h2>Privacy Policy</h2>
+					<p>Effective date: October 30th 2012</p>
 					<p>Your privacy is very important to us. Accordingly, we have developed this Policy in order for you to understand how we collect, use, communicate and disclose and make use of personal information. The following outlines our privacy policy.</p>
 					<ul>
 						<li>Before or at the time of collecting personal information, we will identify the purposes for which information is being collected.</li>
@@ -52,7 +68,6 @@
 						<li>We will make readily available to customers information about our policies and practices relating to the management of personal information. </li>
 					</ul>
 					<p>We are committed to conducting our business in accordance with these principles in order to ensure that the confidentiality of personal information is protected and maintained.</p>
-					<p>Effective date: October 30th 2012</p>
 				</article>
 			</div>
 		</div>
