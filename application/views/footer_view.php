70bc2d1d67e4ec461eb059e8a6c6966dceb4d02e
Roger Qiu <roger.qiu@polycademy.com>
2012-11-10 19:20:15 +1100

    Added ENVIRONMENT check to google analytics to filter out development visits

--- application/views/footer_view.php
+++ application/views/footer_view.php
@@ -63,17 +63,19 @@
 		<script src="<?php echo base_url($links['js_assets']) ?>/jquery.liquid-slider-1.0.min.js"></script>
 
         <script src="<?php echo base_url($links['js_assets']) ?>/main.js"></script>
+		
+		<? if(ENVIRONMENT == 'production'){ ?>
+			<script>
+				var _gaq = _gaq || [];
+				_gaq.push(['_setAccount', '<?=$google_analytics_key?>']);
+				_gaq.push(['_trackPageview']);
 
-        <script>
-			var _gaq = _gaq || [];
-			_gaq.push(['_setAccount', '<?=$google_analytics_key?>']);
-			_gaq.push(['_trackPageview']);
-
-			(function() {
-			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-			})();
-        </script>
+				(function() {
+				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+				})();
+			</script>
+		<? } ?>
     </body>
 </html>
