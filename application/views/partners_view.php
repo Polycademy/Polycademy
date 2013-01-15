28d5caca1f47498cd3e6a6ce11bf8274b9aae1c1
Roger Qiu <roger.qiu@polycademy.com>
2013-01-16 01:42:37 +1100

    Committing the partners page

--- application/views/partners_view.php
+++ application/views/partners_view.php
@@ -7,13 +7,17 @@
 						<h3>Mentors</h3>
 						<p>Our mentors are volunteers who have had extensive experience in the development industry or have relevant business experience. They are here to help you learn!</p>
 						<div class="partner_profiles">
-							<div class="image_rollover_container team_popovers" rel="popover" data-content="<p>BLAH</p>" data-title="Name">
+							<div class="image_rollover_container team_popovers" rel="popover" data-content="<p>Felix has formal qualifications in Economics, and has worked in Canberra for more than half a decade. His expertise includes python and the Google-App-Engine stack, as well as data mining and data visualisation.<br /><br />He is the founder and developer of <a href='www.SharehouseGame.com'>www.SharehouseGame.com</a> and is currently working as a Senior Data Miner in the Australian Government. He is a strong subscriber to the Agile and Lean methodologies.<br /><br />You can check out him out at <a href='http://twitter.com/xlfe'>Twitter</a>.</p>" data-title="Felix Barbalet - Data Scientist">
 								<span class="image_rollover"></span>
-								<img src="<?php echo base_url($links['img_assets']) ?>/team_icon_roger.png" />
+								<img src="<?php echo base_url($links['img_assets']) ?>/mentor_icon_felix.png" />
 							</div>
-							<div class="image_rollover_container team_popovers" rel="popover" data-content="<p>BLAH</p>" data-title="Name">
+							<div class="image_rollover_container team_popovers" rel="popover" data-content="<p>I’m a Creative Developer living in Melbourne, Australia who specializes in web technologies (Flash/Flex, js, html5, css3).  I say ‘Creative Developer’ because my job is to combine both art and functionality in web applications.<br /><br />I started coding at the age of 14 and was creating software professionally by 17 but quickly discovered that there was a fundamental problem with traditional educational systems which needs to be fixed.  I believe that Polycademy is the solution.<br /><br />You can check him out at <a href='https://github.com/mboudreau/'>Github</a> and <a href='http://stackoverflow.com/users/665163/j-a-x'>Stackoverflow</a> or a this <a href='http://michelboudreau.com'>blog</a>.</p>" data-title="Michel Boudreau - Creative Developer">
 								<span class="image_rollover"></span>
-								<img src="<?php echo base_url($links['img_assets']) ?>/team_icon_roger.png" />
+								<img src="<?php echo base_url($links['img_assets']) ?>/mentor_icon_michel.png" />
+							</div>
+							<div class="image_rollover_container team_popovers" rel="popover" data-content="<p>Neil Smith is the  inventor of slidefish which is the first product from his current company Intergami.<br /><br />Neil is a web designer/developer with more than 20 years design, programming, management and marketing experience. Neil has strong project management skills with experience in both small scale business and working with large multi-nationals managing projects through to completion. Projects have been both online and off, from packing and printed products to interactive DVD ROM's, to websites.<br /><br />Neil has built and sold one business.<br /><br />Neil has a degree in Fine Art from the University of Tasmania.<br /><br />You can check him out at <a href='http://www.slidefish.net'>Slidefish</a> and <a href='http://www.linkedin.com/pub/neil-smith/14/481/9a7'>Linkedin</a>.</p>" data-title="Neil Smith - Entrepreneur and Hacker">
+								<span class="image_rollover"></span>
+								<img src="<?php echo base_url($links['img_assets']) ?>/mentor_icon_neil.png" />
 							</div>
 						</div>
 					</section>
@@ -21,9 +25,9 @@
 						<h3>Speakers</h3>
 						<p>Our speakers are people who are interested in running workshops at Polycademy, usually on the topics of entrepreneurship. This will list anyone who has applied or has spoken at Polycademy.</p>
 						<div class="partner_profiles">
-							<div class="image_rollover_container team_popovers" rel="popover" data-content="<p>BLAH</p>" data-title="Name">
+							<div class="image_rollover_container team_popovers" rel="popover" data-content="<p>Interested in being a speaker? Pop us an email!</p>" data-title="Your Name">
 								<span class="image_rollover"></span>
-								<img src="<?php echo base_url($links['img_assets']) ?>/team_icon_roger.png" />
+								<img src="<?php echo base_url($links['img_assets']) ?>/team_icon_teacher.png" />
 							</div>
 						</div>
 					</section>
@@ -31,9 +35,9 @@
 						<h3>Learning Partners</h3>
 						<p>Our learning partners are organisations or companies who have partnered up with Polycademy in order to do interesting projects.</p>
 						<div class="partner_profiles">
-							<div class="image_rollover_container team_popovers" rel="popover" data-content="<p>BLAH</p>" data-title="Name">
+							<div class="image_rollover_container team_popovers" rel="popover" data-content="<p><a href='http://http://www.readspeaker.com/'>ReadSpeaker</a> is the worldwide leader in online text to speech. ReadSpeaker speech-enables online content on the fly in 35+ languages and 100+ voices. More than 5000 corporate, media, government, and nonprofit customers around the world use ReadSpeaker online text-to-speech solutions.<br /><br />ReadSpeaker Australia & NZ are interested in providing Polycademy's students free access to their API so they can learn about web service architecture. ReadSpeaker is also interested in sponsoring Polyhack hackathons.</p>" data-title="ReadSpeaker">
 								<span class="image_rollover"></span>
-								<img src="<?php echo base_url($links['img_assets']) ?>/team_icon_roger.png" />
+								<img src="<?php echo base_url($links['img_assets']) ?>/partner_icon_readspeaker.png" />
 							</div>
 						</div>
 					</section>
